b924b81d572e1605d4f461937fa66a0bbbad4e7e
Hootan <hoootaan@gmail.com>
2018-04-03 14:20:49 +0430

    init porject

---
+++ JBDetection.h
@@ -0,0 +1,15 @@
+//
+//  JBDetection.h
+//  maptest
+//
+//  Created by Hootan Moradi on 4/3/18.
+//  Copyright © 2018 Hootan Moradi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JBDetection : NSObject
+
++ (BOOL)isDeviceJailbroken;
+
+@end
